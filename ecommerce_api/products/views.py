53bbda317bb5ca5ba85e2ac4d8be5a11bee210e7
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-03-25 00:43:33 +0200

    views
    changes to the views

--- ecommerce_api/products/views.py
+++ ecommerce_api/products/views.py
@@ -31,6 +31,7 @@ class OrderItemViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
 class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
